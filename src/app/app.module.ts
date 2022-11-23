eb17b09b04252e53e3de640313bdb940447790f8
ecastiblancor <74267824+ecastiblancor@users.noreply.github.com>
2022-11-23 09:34:27 -0500

    6 files modified, 16 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { NavComponent } from './shared/component/nav/nav.component';
+import { BannerComponent } from './shared/component/banner/banner.component';
+import { BlogComponent } from './shared/component/blog/blog.component';
+import { FeedsComponent } from './shared/component/feeds/feeds.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { formatCurrency } from '@angular/common';
+import { FormComponent } from './shared/component/form/form.component';
+import { PodscatComponent } from './shared/component/podscat/podscat.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MatToolbarModule, MatIconModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavComponent,
+    BannerComponent,
+    BlogComponent,
+    FeedsComponent,
+    FooterComponent,
+    FormComponent,
+    PodscatComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/component/podscat/podscat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-podscat',
+  templateUrl: './podscat.component.html',
+  styleUrls: ['./podscat.component.css']
+})
+export class PodscatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
